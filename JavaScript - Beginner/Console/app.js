460ea2cd14dc1f5af9fa69b3a8cff716e0287e9f
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-11-24 21:37:06 +0300

    add Javascript - Console

---
+++ JavaScript - Beginner/Console/app.js
@@ -0,0 +1,18 @@
+//Output a number
+var a = 20;
+console.log(a);
+
+//Output an array
+console.log(["Ali", "Ahmet"]);
+
+//Output type of a 
+console.log(typeof(a));
+
+//Output warning
+console.warn("This is a warning");
+
+//Output error
+console.error("Can't find this page");
+
+//Clear console
+console.clear();

db65fef5d5df0fd3446ca5deca9da19aaab1d37b
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2024-12-08 20:32:39 +0300

    add Javascript arrow func.

---
+++ JavaScript - Advanced/Arrow Function/app.js
@@ -0,0 +1,13 @@
+const hello = function () {
+  console.log("hello");
+};
+
+const hi = () => {
+  console.log("hi");
+};
+
+const aye = (firstName)=>{
+    console.log("hello", firstName);
+}
+
+const cube = (x)=> x * x * x;

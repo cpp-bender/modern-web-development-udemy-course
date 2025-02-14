6b090adba98db983382552789b6708ad028f36ff
Ahmet Altioglu <ahmetaltioglu@hotmail.com>
2025-02-14 19:53:44 +0300

    update express

--- Express/server.js
+++ Express/server.js
@@ -2,6 +2,16 @@ const express = require("express");
 const app = express();
 const PORT = 5000;
 
-app.listen(PORT, ()=>{
-    console.log("Listening to "+PORT);
+const users = [
+  { id: 1, name: "ahmet", place: "izmir" },
+  { id: 2, name: "mehmet", place: "ankara" },
+];
+
+app.get("/users", (req, res, next)=>{
+    res.json(users);
+});
+
+
+app.listen(PORT, () => {
+  console.log("Listening to " + PORT);
 });
